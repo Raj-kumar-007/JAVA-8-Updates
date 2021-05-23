80e169d5c10dcaffeae0e98268435515588ac2a3
Raj-kumar-007 <jangraraj308@gmail.com>
2021-05-23 19:12:13 +0530

    Changes for endPoint

--- src/main/java/API/APIController.java
+++ src/main/java/API/APIController.java
@@ -1,4 +1,21 @@
 package API;
+import javax.ws.rs.*;
 
+
+@Path("/controller/test")
 public class APIController {
+
+    @GET
+    @Produces("text/plain")
+    public String getWelcomeMsg()
+    {
+        return "Welcome to my first API without Spring";
+    }
+
+    @POST
+    public int getCalculatedSum(@HeaderParam("firstValue") int firstValue,
+                                @HeaderParam("secondValue") int secondValue)
+    {
+        return firstValue + secondValue ;
+    }
 }
